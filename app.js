09a331ea21240676f72db8ff1070472003f135d1
Roger <rogershen5@gmail.com>
2022-09-27 19:31:38 -0700

    use port 80 for heroku

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var app = express();
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+var port = process.env.PORT || 80;
+app.listen(port, () => {
+    console.log("Server running on port " + port);
+}); 
 
 app.get("/url", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
